7499c48dea93368461bfb0d9aa0d4823d7bc7b9c
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2020-10-12 04:58:56 +0300

    Add "file_to_list" function

--- pyaction/general/objects.py
+++ pyaction/general/objects.py
@@ -8,7 +8,7 @@ The main objects of the general field.
 
 """
 
-__all__ = ['json_to_dict', 'dict_to_json']
+__all__ = ['json_to_dict', 'dict_to_json', 'file_to_list']
 
 import json
 
@@ -44,5 +44,25 @@ def dict_to_json(path_to_file, data, encoding='utf-8'):
 
     with open(path_to_file, 'w', encoding=encoding) as js:
         json.dump(data, js)
+
+def file_to_list(path_to_file, keep_duplicates=True, encoding='utf-8'):
+    """Transforms a plain text (txt or csv) file to Python :obj:`list`.
+
+    Args:
+        path_to_file (str): A path to plain text file.
+        encoding (str): File's encoding. Defaults to "utf-8".
+        keep_duplicates (bool): If set to False, the output list will not
+            contain duplicates. Defaults to True.
+
+    Returns:
+        dict: The processed JSON file as a Python :obj:`dict`.
+
+    """
+
+    handler = open(path_to_file, "r", encoding=encoding)
+    lines = [line.replace("\n", "") for line in handler.readlines()]
+    handler.close()
+
+    return lines if keep_duplicates else list(dict.fromkeys(lines))
 
 #-----------------------------------------------------------------------------
--- tests/general/test_objects.py
+++ tests/general/test_objects.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from pyaction.general.objects import json_to_dict, dict_to_json
+from pyaction.general.objects import *
 import unittest
 import sys
 import os
@@ -19,7 +19,20 @@ class TestGeneralObjects(unittest.TestCase):
     def test_dict_to_json(self):
         test_dict = {"key1": "value1", "key2": "value2"}
         file_name = 'example_json_2.json'
-        dict_to_json(os.path.join(files, file_name), data=test_dict)
-        self.assertIn(file_name, os.listdir(files))
+        file_path = os.path.join(files, file_name)
+        dict_to_json(file_path, data=test_dict)
+        result = file_name in os.listdir(files)
+        os.remove(file_path)
+        self.assertTrue(result)
+
+    def test_file_to_list_with_duplicates(self):
+        file_name = 'example_txt.txt'
+        output = file_to_list(os.path.join(files, file_name))
+        self.assertEqual(output, ['1', '1', '2', '3'])
+
+    def test_file_to_list_without_duplicates(self):
+        file_name = 'example_txt.txt'
+        output = file_to_list(os.path.join(files, file_name), False)
+        self.assertEqual(output, ['1', '2', '3'])
 
 #-----------------------------------------------------------------------------
