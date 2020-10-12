e2ba66083a6c91e74c57785b0ba05f0a9b99133c
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2020-10-12 04:18:10 +0300

    Add tests to "dict_to_json" function

--- tests/general/test_objects.py
+++ tests/general/test_objects.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from pyaction.general.objects import json_to_dict
+from pyaction.general.objects import json_to_dict, dict_to_json
 import unittest
 import sys
 import os
@@ -15,5 +15,11 @@ class TestGeneralObjects(unittest.TestCase):
     def test_json_to_dict(self):
         data = json_to_dict(os.path.join(files, 'example_json.json'))
         self.assertEqual(data, {"key1": "value1", "key2": "value2"})
+
+    def test_dict_to_json(self):
+        test_dict = {"key1": "value1", "key2": "value2"}
+        file_name = 'example_json_2.json'
+        dict_to_json(os.path.join(files, file_name), data=test_dict)
+        self.assertIn(file_name, os.listdir(files))
 
 #-----------------------------------------------------------------------------
